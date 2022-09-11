ee7ae831f7ade84d0c4b4d93848d12238b5bfc10
Daniel Lu <ludaniel20110@outlook.com>
2022-09-11 08:49:36 +0000

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { string } from 'rollup-plugin-string'
  * @type {import('vite').UserConfig}
  */
 export default {
+  build: {
+    // generate manifest.json in outDir
+    manifest: true
+  },
   plugins: [
     string({
       include: '**/*.txt',
